a0824253c5aaf105439167f48adde78b82eec602
Pariek6 <patrickfrumau@hotmail.com>
2017-09-15 12:06:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var one       = 1;
+var two       = true;
+var three     = false;
+var four      = "Nummer 4";
+var five      = 5;
+var six       = 6;
+var seven     = 7;
+var eight     = 8;
+var nine      = 9;
+var ten       = 10;
+var eleven    = 11;
+var twelve    = 12;
+var thirtheen = 13;
+var fourtheen = 14;
+var fiftheen  = 15;
+
+var numberplusnumber   = five + six;
+var numberplusstring   = 
